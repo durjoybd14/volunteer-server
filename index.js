ff0c3c0ab274ef4b42cca9ca9f33bc618221b7ba
nrd141913 <nrd141913@gmail.com>
2021-03-30 14:58:22 +0600

    server deployed

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ const port = process.env.PORT || 5055
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res)=>{
+  req.send('Hello World')
+})
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ghclx.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-
-console.log(uri)
-
+// console.log(uri)
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
@@ -22,7 +24,7 @@ client.connect(err => {
   const eventCollection = client.db("volunteer").collection("events");
   // perform actions on the collection object
 
- 
+
 
   app.get('/events', (req, res) => {
     eventCollection.find().toArray((err, items) => {
@@ -42,13 +44,13 @@ client.connect(err => {
   })
 
 
-app.delete('/deleteEvent/:id', (req, res) => {
-  eventCollection.deleteOne({ _id: ObjectId(req.params.id) })
-  .then(result => {
-    res.send(result.deletedCount > 0);
+  app.delete('/deleteEvent/:id', (req, res) => {
+    eventCollection.deleteOne({ _id: ObjectId(req.params.id) })
+      .then(result => {
+        res.send(result.deletedCount > 0);
 
+      })
   })
-})
 
 });
 
