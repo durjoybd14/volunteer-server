ebbdc998b3cb48899a5d1a1fe5a735b8df09d988
nrd141913 <nrd141913@gmail.com>
2021-03-30 15:01:56 +0600

    done

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res)=>{
-  req.send('Hello World')
+  res.send('Hello World')
 })
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ghclx.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
